cbcdc65a8eaab0384ed7fed9ba17e41e621287ed
yuriikaplunivskyi <yurii.kaplunivskyi@gmail.com>
2023-05-07 00:48:17 +0300

    feat: added state for "del" and "add"

--- src/components/employees-add-form/employees-add-form.js
+++ src/components/employees-add-form/employees-add-form.js
@@ -16,13 +16,24 @@ class EmployeesAddForm extends Component {
         })
     }
 
+    onSubmit = (e) => {
+        e.preventDefault();
+        this.props.onAdd(this.state.name, this.state.salary);
+        this.setState({
+            name: '',
+            salary: ''
+        })
+    }
+
     render() { 
         const {name, salary} = this.state;
+        
         return(
             <div className="app-add-form">
                 <h3>Add new employee</h3>
                 <form
-                    className="add-form d-flex">
+                    className="add-form d-flex"
+                    onSubmit={this.onSubmit}>
                     <input type="text"
                         className="form-control new-post-label"
                         placeholder="What is the name?"
